78828450ac8bddcfccd9cfff8103251ed14456ea
ZeldaHuang <1520787127@qq.com>
2020-12-23 01:04:23 +0800

    购物车底部bar

--- app/component/cartItem.js
+++ app/component/cartItem.js
@@ -10,43 +10,58 @@ import LocalImg from '../images/images'
 function CartItem (props){
 const [checked, setChecked] = React.useState(false);
 const [visible, setVisible] = React.useState(false);
+const [isFavor,setIsFavor]=React.useState(false);
 const [num,setNum]=React.useState(0);
 const showModal = () => setVisible(true);
 const hideModal = () => setVisible(false);
     return(
       <Card style={{height:100,borderRadius:20,marginTop:10}}>
-      
-          <View style={styles.container}>
-            <TouchableOpacity onPress={()=>{setChecked(!checked)}} style={{alignItems:"center",flexDirection: 'row',marginBottom:px2dp(15)}} >
+        <View style={styles.container}>
+          <TouchableOpacity onPress={()=>{setChecked(!checked)}} style={{alignItems:"center",flexDirection: 'row',marginBottom:px2dp(15)}} >
             <AntdIcon name={checked==true?"checkcircle":"checkcircleo"} size={20} style={{marginLeft:20}}></AntdIcon>
             <Image
+
             source={LocalImg.book1 }
             style={styles.thumbnail }
-           /> 
-              </TouchableOpacity>
-            <View style={styles.rightContainer}>
-              
-              <TouchableOpacity style={{alignSelf:"center",marginLeft:px2dp(5)}}>
-                <Text style={styles.title}>《三体三部曲》</Text>
-                <Text style={styles.price}>￥99</Text>
-              </TouchableOpacity>
-              <View style={{flexDirection:"row",alignItems:"center",marginBottom:px2dp(15)}}>
-                <TouchableOpacity style={{marginHorizontal:px2dp(12)}}
-              onPress={()=>{setNum(num+1)}}>
-                <AntdIcon name="pluscircle" size={px2dp(22)}></AntdIcon>
-              </TouchableOpacity >
-              <Text style={{fontSize:20}}>{num}</Text>
+          /> 
+          </TouchableOpacity>
+          <View style={styles.rightContainer}>
+            <TouchableOpacity style={{alignSelf:"center",marginLeft:px2dp(5)}}
+            onPress={()=>{showModal()}}>
+              <Text style={styles.title}>《三体三部曲》</Text>
+              <Text style={styles.price}>￥99</Text>
+            </TouchableOpacity>
+            <View style={{flexDirection:"row",alignItems:"center",marginBottom:px2dp(15)}}>
               <TouchableOpacity style={{marginHorizontal:px2dp(12)}}
-              onPress={()=>{setNum(num-1)}}>
-                <AntdIcon name="minuscircle" size={px2dp(22)}></AntdIcon>
-              </TouchableOpacity>
-              </View>
+            onPress={()=>{setNum(num+1)}}>
+              <AntdIcon name="pluscircle" size={px2dp(22)}></AntdIcon>
+            </TouchableOpacity >
+            <Text style={{fontSize:20}}>{num}</Text>
+            <TouchableOpacity style={{marginHorizontal:px2dp(12)}}
+            onPress={()=>{setNum(num-1)}}>
+              <AntdIcon name="minuscircle" size={px2dp(22)}></AntdIcon>
+            </TouchableOpacity>
             </View>
-           
-          </View>
+          </View> 
+        </View>
+        <Portal>
+          <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={styles.containerStyle}>
+            <ScrollView>
+            <TouchableOpacity 
+            onPress={()=>{}}>{isFavor?(<AntdIcon name={'star'} size={px2dp(24)} color={'#0865b5'} style={{alignSelf:"flex-end"}}/>)
+                :(<AntdIcon name={'staro'} size={px2dp(24)} color={'#0865b5'} style={{alignSelf:"flex-end"}}/>)}
+              </TouchableOpacity>
+              <Text style={{fontSize:18,alignSelf:"center"}}>《三体三部曲》(《三体三部曲》著)</Text>
+            <Image
+                source={LocalImg.book1 }
+                style={styles.detailsImage}
+            />
+            <Text style={styles.priceStyle}>￥99</Text>
+            <Text>fdsfdsfd</Text>
+            </ScrollView>
+          </Modal>
+        </Portal>
       </Card>
-        
-        
     );
 };
 export default CartItem;
--- app/pages/shopCart.js
+++ app/pages/shopCart.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { FlatList, StyleSheet, Text, View ,TouchableOpacity} from 'react-native';
 import PropTypes from 'prop-types';
-import { Provider,Modal, Portal } from 'react-native-paper';
+import AntdIcon from 'react-native-vector-icons/AntDesign';
+import { Provider,Modal, Portal,Colors,Card,Button} from 'react-native-paper';
 import CartItem from "../component/cartItem"
 import Data from '../data';
 import {deviceWidth, isIOS, px2dp} from '../util/index';
 function ShopCart ({ route, navigation }){
+    const [allChecked,setAllChecked] =React.useState(false);
     return (
         <Portal.Host>
             <View style={styles.container}>
                 <CartItem></CartItem>
                 <CartItem></CartItem>
             </View>
+            <View style={styles.bottomBar}>
+                <TouchableOpacity onPress={()=>{setAllChecked(!allChecked)}}
+                style={{alignItems:"center",flexDirection: 'row',marginLeft:px2dp(10),width:px2dp(50)}} >
+                    <AntdIcon name={allChecked==true?"checkcircle":"checkcircleo"} size={20} style={{marginHorizontal:px2dp(10)}}></AntdIcon>
+                    <Text style={{fontSize:15}}>全选</Text>
+                </TouchableOpacity>
+                <View style={{marginLeft:deviceWidth-px2dp(280),flexDirection:"row"}}>
+                    <Text style={{textAlign:"center",fontSize:15,alignSelf:"center",marginHorizontal:px2dp(20)}}>
+                        总计:
+                        <Text style={{color:Colors.orange700}}>￥99</Text>
+                    </Text>
+                    <Button mode="contained" onPress={() => console.log('Pressed')} style={styles.bottomButton}>
+                    结算
+                     </Button>
+                </View>
+                
+            </View>
         </Portal.Host>
-    
     );
 }
 
@@ -28,4 +46,20 @@ const styles = StyleSheet.create({
       fontSize: 18,
       height: 44,
     },
+    bottomBar:{
+        alignSelf:"center",
+        alignItems:"center",
+        flexDirection:"row",
+        height:px2dp(50),
+        width:deviceWidth,
+        backgroundColor:Colors.white
+    },
+    bottomButton:{
+        width:px2dp(100),
+        height:px2dp(40),
+        alignSelf:"auto",
+        alignItems:"center",
+        borderRadius:px2dp(20),
+        backgroundColor:Colors.orange700
+    }
   });
